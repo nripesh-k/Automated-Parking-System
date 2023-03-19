0229c0b1df7a72d15777e08777a7ec8b896f3f4b
nripesh-k <nripeshk8@gmail.com>
2023-03-19 18:13:49 +0545

    For evaluating the Recognition Model

---
+++ evaluateModel.py
@@ -0,0 +1,11 @@
+import model as mdl
+import prepareData as pd
+
+if __name__ == "__main__":
+    recognitionModel = mdl.model()
+    recognitionModel.load_weights('weights.h5')
+
+    testData, testLabel = pd.readHDF5('test.h5')
+    testScore = recognitionModel.evaluate(testData, testLabel, verbose=0)
+    print(f'Test Loss: {testScore[0]}')
+    print(f'Test Accuracy: {testScore[1]*100}%')
