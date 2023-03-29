c199532f8a4875c6eda0c22119ee403da5625533
Sajan491 <sajanmahat491@gmail.com>
2023-03-29 17:08:52 +0545

    flask init

--- ParkingSpace/main.py
+++ ParkingSpace/main.py
@@ -2,6 +2,10 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Flask to show current status in a screen
+from flask import Flask, render_template, Response
+app = Flask(__name__)
+
 from util import get_parking_spots, empty_or_not
 
 # To compare two instances for each crop and look for change
@@ -29,64 +33,88 @@ previous_frame = None # To compare each frames
 frame_count = 0
 step = 60
 
-ret = True
-while ret:
-    # Looping video
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+def process():
+    global frame_count, step, previous_frame, spot_status, diffs
+    ret = True
+    while ret:
+        # Looping video
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    #Read video
-    rets, frame = cap.read()
+        #Read video
+        rets, frame = cap.read()
 
-    if frame_count % step == 0 and previous_frame is not None:
+        if frame_count % step == 0 and previous_frame is not None:
+            for spot_indx, spot in enumerate(spots):
+                x1, y1, w, h = spot
+
+                spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
+
+                diffs[spot_indx] = calc_diff(spot_crop, previous_frame[y1:y1 + h, x1:x1 + w, :])
+
+            # print([diffs[j] for j in np.argsort(diffs)][::-1])
+
+        if frame_count % step == 0:
+            if previous_frame is None:
+                arr_ = range(len(spots))
+            else:
+                arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs) > 0.4]
+            for spot_indx in arr_:
+                spot = spots[spot_indx]
+                x1, y1, w, h = spot
+
+                spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
+
+                spot_status = empty_or_not(spot_crop)
+
+                spots_status[spot_indx] = spot_status
+
+        # if frame_count % step == 0:
+            previous_frame = frame.copy()
+
+        # Spot red or green
         for spot_indx, spot in enumerate(spots):
-            x1, y1, w, h = spot
+            spot_status = spots_status[spot_indx]
+            x1, y1, w, h = spots[spot_indx]
 
-            spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
+            if spot_status:
+                frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+            else:
+                frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
 
-            diffs[spot_indx] = calc_diff(spot_crop, previous_frame[y1:y1 + h, x1:x1 + w, :])
+        # Available spots text
+        cv2.rectangle(frame, (80, 20), (550, 80), (0, 0, 0), -1)
+        cv2.putText(frame, 'Available spots: {} / {}'.format(str(sum(spots_status)), str(len(spots_status))), (100, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-        # print([diffs[j] for j in np.argsort(diffs)][::-1])
+        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
 
-    if frame_count % step == 0:
-        if previous_frame is None:
-            arr_ = range(len(spots))
-        else:
-            arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs) > 0.4]
-        for spot_indx in arr_:
-            spot = spots[spot_indx]
-            x1, y1, w, h = spot
+        frame_count += 1 #increment just by one each frame running
 
-            spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
+        # Sending every update to flask server
+        processed_frame = frame
+        # Convert the processed frame to a JPEG image
+        ret, jpeg = cv2.imencode('.jpg', processed_frame)
+        # Convert the JPEG image to bytes
+        data = jpeg.tobytes()
+        # Yield the bytes as a multipart response
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + data + b'\r\n')
+        
 
-            spot_status = empty_or_not(spot_crop)
+    cap.release()
+    cv2.destroyAllWindows()
 
-            spots_status[spot_indx] = spot_status
+process()
 
-    # if frame_count % step == 0:
-        previous_frame = frame.copy()
+@app.route('/')
+def index():
+    render_template('index.html')
+    return Response(process(),
+                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    # Spot red or green
-    for spot_indx, spot in enumerate(spots):
-        spot_status = spots_status[spot_indx]
-        x1, y1, w, h = spots[spot_indx]
-
-        if spot_status:
-            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
-        else:
-            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
-
-    # Available spots text
-    cv2.rectangle(frame, (80, 20), (550, 80), (0, 0, 0), -1)
-    cv2.putText(frame, 'Available spots: {} / {}'.format(str(sum(spots_status)), str(len(spots_status))), (100, 60),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
-    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
-
-    frame_count += 1 #increment just by one each frame running
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    app.run()
